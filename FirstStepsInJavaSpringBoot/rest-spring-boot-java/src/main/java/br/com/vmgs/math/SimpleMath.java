0b94bc82f171a6299384289a3b5234498a90a585
Vaniza <vaniza@outlook.com>
2023-08-09 14:24:36 -0300

    the math

---
+++ FirstStepsInJavaSpringBoot/rest-spring-boot-java/src/main/java/br/com/vmgs/math/SimpleMath.java
@@ -0,0 +1,35 @@
+package br.com.vmgs.math;
+
+public class SimpleMath {
+
+	public Double sum(Object covertToDouble, Object covertToDouble2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double subtraction(Object covertToDouble, Object covertToDouble2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double multiplication(Object covertToDouble, Object covertToDouble2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double division(Object covertToDouble, Object covertToDouble2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double mean(Object covertToDouble, Object covertToDouble2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double squareRoot(Object covertToDouble) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
